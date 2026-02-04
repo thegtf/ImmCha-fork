82e9a9fbd0b99693db33872f1983b72ab7d4a67b
Paul Pham <>
2026-02-03 22:03:55 -0600

    Cat attacking doesn't work yet.

--- java/src/teacher05a/CatAttacker.java
+++ java/src/teacher05a/CatAttacker.java
@@ -1,7 +1,6 @@
 package teacher05a;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class CatAttacker extends BabyRat {
 
@@ -11,6 +10,44 @@ public class CatAttacker extends BabyRat {
     }
 
     public void doAction() throws GameActionException {
+        // search for cheese
+        RobotInfo[] nearbyInfos = rc.senseNearbyRobots();
+
+        System.out.println("Sensed " + nearbyInfos.length + " robots");
+        MapLocation catLoc = null;
+        for (RobotInfo info : nearbyInfos) {
+            if (info.getType().isCatType()) {
+                Direction toCat = rc.getLocation().directionTo(catLoc);
+                catLoc = info.getLocation();                
+                if (rc.canTurn(toCat)) {
+                    rc.turn(toCat);
+                    break;
+                }
+            }
+
+        }
+
+        MapLocation nextLoc = rc.adjacentLocation(rc.getDirection());
+        if (rc.canRemoveDirt(nextLoc)) {
+            rc.removeDirt(nextLoc);
+        }
+
+        if (rc.canMoveForward()) {
+            rc.moveForward();
+            rc.setIndicatorString("Finding cat.");
+        } else {
+            d = directions[rand.nextInt(directions.length-1)];
+            if (rc.canTurn()) {
+                rc.turn(d);
+            }
+            rc.setIndicatorString("Blocked while finding cat, turning " + d.toString());
+            return;
+        }
+
+        if ((catLoc != null) && rc.canAttack(catLoc)) {
+            rc.attack(catLoc);
+            rc.setIndicatorString("Victory or death!");
+        }
 
     }
 
