8bba9b7b2c4ecdf8229e7190140f48c2c808dfb4
jake <gabriel@Userrs-iMac.local>
2026-02-26 19:10:42 -0800

    bugNav0 added to sub player

--- java/src/immortal_chariot/RobotSubPlayer.java
+++ java/src/immortal_chariot/RobotSubPlayer.java
@@ -2,6 +2,7 @@ package immortal_chariot;
 
 import java.util.Random;
 
+import apple.laf.JRSUIConstants.Direction;
 import battlecode.common.*;
 
 public abstract class RobotSubPlayer {
@@ -34,9 +35,39 @@ public abstract class RobotSubPlayer {
                 rc.turn(d);
             }
         }
-
     }
 
+    public void bugNav0(RobotController rc, MapLocation goal) throws GameActionException {
+        MapLocation here = rc.getLocation();
+        Direction toGoal = here.directionTo(goal);
+        Direction left = rc.getDirection().rotateLeft();
+        boolean followingWall = false;
+
+            if (rc.canMove(toGoal)) { //if can move to goal
+                rc.turn(toGoal);
+                rc.moveForward();
+                followingWall = false;
+            } else {
+                followingWall = true;
+            }
+
+            if (followingWall) { //if can't, follow wall
+                if (rc.canMoveForward()) {
+                    rc.moveForward();
+                } else {
+                    if (rc.canTurn(left)) {
+                        rc.turn(left);
+                    }
+                }
+                if (rc.canMove(toGoal)) {
+                    followingWall = false;
+                }
+            }
+
+            if (here.distanceSquaredTo(goal)<=4 && rc.canMove(toGoal)) {
+                break;
+            }
+    }
     
     // public void writeRatKingCount(int ratKingCount) throws GameActionException {
     //     rc.writeSharedArray(0, ratKingCount);
